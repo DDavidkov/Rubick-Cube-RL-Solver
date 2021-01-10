ec1b0bbbd29955ce3df8499133b185f37d511345
cacao-macao <pavel.z.tashev@protonmail.com>
2021-01-10 12:00:49 +0200

    static methods and vars

--- src/cube_model_naive.py
+++ src/cube_model_naive.py
@@ -5,33 +5,78 @@ from collections import namedtuple
 class Cube:
     """
     A concrete class representation of a Rubik's Cube.
-    The cube is represented as a flattened 2D matrix.
+    The state of the cube is represented as a flattened 2D matrix.
 
-    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6     # 1 is orange
-    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6     # 2 is green  
-    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6     # 3 is red
-                                            # 4 is white
-                                            # 5 is blue
-                                            # 6 is yellow
+    0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5     # 0 is orange
+    0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5     # 1 is green  
+    0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5     # 2 is red
+                                            # 3 is white
+                                            # 4 is blue
+                                            # 5 is yellow
+    
+    Every color number is additionally represented as a one-hot
+    vector, thus making the state a 3D tensor of shape (3, 18, 6).
     """
-    #--------  nested named tuples --------#
+    #----------- class variables ----------#
     Side = namedtuple("Side", ["LEFT", "FRONT", "RIGHT", "BACK", "UP", "DOWN"])(
                                 0, 1, 2, 3, 4, 5)
     Direction = namedtuple("Direction", ["ANTI_CLOCK", "CLOCK"])(0, 1)
-    Color = namedtuple("Color", ["ORANGE", "GREEN", "RED", "BLUE", "WHITE", "YELLOW"])(
+    Color = namedtuple("Color", ["ORANGE", "GREEN", "RED", "WHITE", "BLUE", "YELLOW"])(
                                 0, 1, 2, 3, 4, 5)
 
-    #---------- cube initializer ----------#
+    num_actions = len(Side) * len(Direction)
+    action_space = np.arange(num_actions, dtype=int)
+    terminal_state = np.hstack([np.full((3,3), _col) for _col in Color])
+    terminal_state = np.array(np.expand_dims(
+                                    terminal_state, -1) == np.arange(len(Color)),
+                                    dtype=np.float32)
+
+    #------------ class methods -----------#
+    @classmethod
+    def is_terminal(cls, state):
+        """ Return True if the state is the terminal state for the environment.
+        
+        @param state (Array): A state of the environment.
+        @returns result (bool): True if the state is terminal for the environment.
+                                Otherwise False.
+        """
+        return np.all(state == cls.terminal_state)
+
+    @classmethod
+    def reward(cls, state):
+        """ Return the immediate reward on transition to state `state`.
+
+        @param state (Array): A state of the environment.
+        @returns reward (int): Reward on trasition to state `state`.
+        """
+        return 1 if cls.is_terminal(state) else -1
+
+    @classmethod
+    def expand_state(cls, state):
+        """ Given a state use the model of the environment to obtain
+        the descendants of that state and their respective rewards.
+        Return the descendants and the rewards.
+
+        @param state (Array): A state of the environment.
+        @returns children (Array[state]): A numpy array of shape (num_acts, state.shape) giving
+                                            the children of the input state.
+        @returns rewards (Array): A numpy array of shape (num_acts, 1) containing the
+                                    respective rewards.
+        """
+        _cube = cls()
+        _cube.state = state
+        children = [_cube._take_action[act]() for act in cls.action_space]
+        rewards = [cls.reward(_c) for _c in children]
+        return np.stack(children), np.vstack(rewards)   # rewards shape is (num_acts, 1)
+
+    #----------- env initializer ----------#
     def __init__(self):
-        """ Initialize a cube object. """
-        self._num_actions = len(self.Side) * len(self.Direction)
-        self._action_space = np.arange(self._num_actions, dtype=int)
-        self._terminal_state = np.hstack([np.full((3,3), _col) for _col in self.Color])
-        self._terminal_state = one_hot(self._terminal_state)
+        """ Initialize an environment object. """
+        # # Create the action space.
+        # self._num_actions = len(self.Side) * len(self.Direction)
+        # self._action_space = np.arange(self._num_actions, dtype=int)
 
-        self._state = None
-        self.reset()
-
+        # Enumerate all valid actions from the action space.
         self._take_action = {0: self._left_anticlock,
                              1: self._left_clock,
                              2: self._front_anticlock,
@@ -45,6 +90,29 @@ class Cube:
                              10: self._down_anticlock,
                              11: self._down_clock}
 
+        # # Create the terminal state and represent the colors of the stickers as one-hot vectors.
+        # self._terminal_state = np.hstack([np.full((3,3), _col) for _col in self.Color])
+        # self._terminal_state = np.array(np.expand_dims(
+        #                                     self._terminal_state, -1) == np.arange(len(self.Color)),
+        #                                     dtype=np.float32)
+
+        # Set the current state to None.
+        self._state = None
+
+        # Reset the environment.
+        self.reset()
+
+    #---------- property methods ----------#
+    @property
+    def state(self):
+        """ Return the current state of the environment. """
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        """ Set the current state of the environment. """
+        self._state = new_state.copy()
+
     #----------- public methods -----------#
     def step(self, act):
         """ Make a single step taking the specified action.
@@ -54,7 +122,7 @@ class Cube:
         @returns reward (int): An integer representing the reward after arriving at the next state.
         @returns done (bool): A boolen indicating whether this is a terminal state.
         """
-        if act not in self._action_space:
+        if act not in self.action_space:
             raise Exception("Unknown action %s", act)
 
         # Observe the next state after taking action `act`.
@@ -65,33 +133,38 @@ class Cube:
 
         # Check if this is the final state.
         done = self.is_solved()
-        reward = 1 if done else -1
+        reward = self.reward(self._state)
 
         return next_state, reward, done
 
-    def set_random_state(self):
-        """ Set the current state of the cube to a random valid state. """
+    def set_random_state(self, seed=None, scrambles=None):
+        """ Set the current state of the environment to a random valid state. """
         self.reset()
-        for _ in range(100):
-            act = np.random.randint(low=0, high=12)
-            self.step(act)
+
+        np.random.seed(seed=seed)
+        if scrambles is None:
+            scrambles = 100
+        acts = np.random.randint(low=0, high=self.num_actions, size=scrambles)
+        for a in acts:
+            self.step(a)
 
     def reset(self):
-        """ Set the current state of the cube to the terminal state. """
-        self._state = self._terminal_state.copy()
+        """ Set the current state of the environment to the terminal state. """
+        self._state = self.terminal_state.copy()
 
-    def is_valid(self):
-        """ Return True if the current state is a valid state for the cube. """
-        print("Not implemented!")
-        return True
+    # def is_valid(self):
+    #     """ Return True if the current state is a valid state for the environment. """
+    #     print("Not implemented!")
+    #     return True
 
     def is_solved(self):
-        """ Return True if the current state is the terminal state for the cube. """
-        return np.all(self._state == self._terminal_state)
-    
-    def is_terminal(self, state):
-        """ Return True if the state is the terminal state for the cube. """
-        return np.all(state == self._terminal_state)
+        """ Return True if the current state is the terminal state for the environment. """
+        return self.is_terminal(self._state)
+        # return np.all(self._state == self._terminal_state)
+
+    # def is_terminal(self, state):
+    #     """ Return True if the state is the terminal state for the cube. """
+    #     return np.all(state == self._terminal_state)
 
     #----------- private methods ----------#
     def _left_anticlock(self):
@@ -151,9 +224,9 @@ class Cube:
 
         # Rotate the adjecent rows and columns.
         next_state[:, 3 * l + 2] = self._state[-1, 3 * u : 3 * (u + 1)][::-1]   # flip
-        next_state[0, 3 * d : 3 * (d + 1)] = self._state[:, 3 * l + 2] 
+        next_state[0, 3 * d : 3 * (d + 1)] = self._state[:, 3 * l + 2]
         next_state[:, 3 * r] = self._state[0, 3 * d : 3 * (d + 1)][::-1]        # flip
-        next_state[-1, 3 * u : 3 * (u + 1)] = self._state[:, 3 * r] 
+        next_state[-1, 3 * u : 3 * (u + 1)] = self._state[:, 3 * r]
 
         return next_state
 
@@ -171,9 +244,9 @@ class Cube:
         next_state[:, 3*side:3*(side+1)] = np.rot90(self._state[:, 3*side:3*(side+1)], k=(-1)**dir)
 
         # Rotate the adjecent rows and columns.
-        next_state[:, 3 * l + 2] = self._state[0, 3 * d : 3 * (d + 1)] 
+        next_state[:, 3 * l + 2] = self._state[0, 3 * d : 3 * (d + 1)]
         next_state[0, 3 * d : 3 * (d + 1)] = self._state[:, 3 * r][::-1]        # flip
-        next_state[:, 3 * r] = self._state[-1, 3 * u : 3 * (u + 1)] 
+        next_state[:, 3 * r] = self._state[-1, 3 * u : 3 * (u + 1)]
         next_state[-1, 3 * u : 3 * (u + 1)] = self._state[:, 3 * l + 2][::-1]   # flip
 
         return next_state
@@ -192,8 +265,8 @@ class Cube:
         next_state[:, 3*side:3*(side+1)] = np.rot90(self._state[:, 3*side:3*(side+1)], k=(-1)**dir)
 
         # Rotate the adjecent rows and columns.
-        next_state[:, 3 * f + 2] = self._state[:, 3 * u + 2] 
-        next_state[:, 3 * d + 2] = self._state[:, 3 * f + 2] 
+        next_state[:, 3 * f + 2] = self._state[:, 3 * u + 2]
+        next_state[:, 3 * d + 2] = self._state[:, 3 * f + 2]
         next_state[:, 3 * b] = self._state[:, 3 * d + 2][::-1]      # flip
         next_state[:, 3 * u + 2] = self._state[:, 3 * b][::-1]      # flip
 
@@ -213,10 +286,10 @@ class Cube:
         next_state[:, 3*side:3*(side+1)] = np.rot90(self._state[:, 3*side:3*(side+1)], k=(-1)**dir)
 
         # Rotate the adjecent rows and columns.
-        next_state[:, 3 * f + 2] = self._state[:, 3 * d + 2] 
+        next_state[:, 3 * f + 2] = self._state[:, 3 * d + 2]
         next_state[:, 3 * d + 2] = self._state[:, 3 * b][::-1]      # flip
         next_state[:, 3 * b] = self._state[:, 3 * u + 2][::-1]      # flip
-        next_state[:, 3 * u + 2] = self._state[:, 3 * f + 2] 
+        next_state[:, 3 * u + 2] = self._state[:, 3 * f + 2]
 
         return next_state
 
@@ -234,9 +307,9 @@ class Cube:
         next_state[:, 3*side:3*(side+1)] = np.rot90(self._state[:, 3*side:3*(side+1)], k=(-1)**dir)
 
         # Rotate the adjecent rows and columns.
-        next_state[:, 3 * r + 2] = self._state[0, 3 * u : 3 * (u + 1)] 
-        next_state[-1, 3 * d : 3 * (d + 1)] = self._state[:, 3 * r + 2] 
-        next_state[:, 3 * l] = self._state[-1, 3 * d : 3 * (d + 1)][::-1]       # flip
+        next_state[:, 3 * r + 2] = self._state[0, 3 * u : 3 * (u + 1)]
+        next_state[-1, 3 * d : 3 * (d + 1)] = self._state[:, 3 * r + 2][::-1]       # flip
+        next_state[:, 3 * l] = self._state[-1, 3 * d : 3 * (d + 1)]
         next_state[0, 3 * u : 3 * (u + 1)] = self._state[:, 3 * l][::-1]        # flip
 
         return next_state
@@ -256,9 +329,9 @@ class Cube:
 
         # Rotate the adjecent rows and columns.
         next_state[:, 3 * r + 2] = self._state[-1, 3 * d : 3 * (d + 1)][::-1]   # flip
-        next_state[-1, 3 * d : 3 * (d + 1)] = self._state[:, 3 * l] 
+        next_state[-1, 3 * d : 3 * (d + 1)] = self._state[:, 3 * l]
         next_state[:, 3 * l] = self._state[0, 3 * u : 3 * (u + 1)][::-1]        # flip
-        next_state[0, 3 * u : 3 * (u + 1)] = (self._state[:, 3 * r + 2]) 
+        next_state[0, 3 * u : 3 * (u + 1)] = (self._state[:, 3 * r + 2])
 
         return next_state
 
@@ -334,6 +407,29 @@ class Cube:
 
         return next_state
 
+    #----------- static methods -----------#
+    @staticmethod
+    def plot_state(state):
+        """
+        Given an environment state, prints the state in pretty form.
+
+              4 4 4                 # 0 is orange
+              4 4 4                 # 1 is green
+              4 4 4                 # 2 is red
+        0 0 0 1 1 1 2 2 2 3 3 3     # 3 is white
+        0 0 0 1 1 1 2 2 2 3 3 3     # 4 is blue
+        0 0 0 1 1 1 2 2 2 3 3 3     # 5 is yellow
+              5 5 5            
+              5 5 5            
+              5 5 5            
+        """
+        result = np.zeros((9, 12), dtype=int)
+        state = np.argmax(state, axis=-1)
+        result[3:6, :] = state[:, :12]
+        result[0:3, 3:6] = state[:, 12:15]
+        result[6:9, 3:6] = state[:, 15:]
+        print(result)
+
 
 ##############################
 def plot_state(state):
@@ -357,14 +453,14 @@ def expand_state(state):
     cube = Cube()
     children = []
     rewards = []
-    for act in cube._action_space:
+    for act in cube.action_space:
         cube._state = state
         child, reward, _ = cube.step(act)
         children.append(child)
         rewards.append(reward)
     return np.stack(children), np.stack(rewards)
 
-def one_hot(state):
-    return np.array(np.expand_dims(state, -1) == np.arange(6), dtype=float)
+# def one_hot(state):
+#     return np.array(np.expand_dims(state, -1) == np.arange(6), dtype=float)
 
 #
