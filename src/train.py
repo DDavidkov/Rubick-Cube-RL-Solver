bdc4aa1c24ad0e984ffc6d309e8a4d87baf19aba
cacao-macao <pavel.z.tashev@protonmail.com>
2021-01-12 20:29:24 +0200

    minor fixes

--- src/train.py
+++ src/train.py
@@ -8,7 +8,8 @@ from functools import partial
 from math import ceil
 
 
-from fcnn import fc_net as model_fn
+# from fcnn import fc_net as model_fn
+from cnn import conv_net as model_fn
 
 
 
@@ -34,12 +35,12 @@ def expand_states(states, env):
     return children, rewards
 
 
-def generate_episodes(rng, env, episodes, k_max):
+def generate_episodes(rng, env, episodes, k):
     """ Generate a random sequence of states starting from the solved state.
 
     @param env (Cube Object): A Cube object representing the environment.
     @param episodes (int): Number of episodes to be created.
-    @param k_max (int): Maximum length of backward moves.
+    @param k (int): Length of backward moves.
     @returns states (Array[state]): Sequence of generated states. The shape of the array
                                     is (episodes * k_max, state.shape).
     @returns weights (Array): Array of weights. w[i] corresponds to the weight of states[i].
@@ -58,10 +59,8 @@ def generate_episodes(rng, env, episodes, k_max):
     # Create `episodes` number of episodes.
     for _ in range(episodes):
         cube.reset()
-        rng, _, act_rng = jax.random.split(rng, num=3)
-        # k = int(jax.random.randint(k_rng, shape=(1,), minval=1, maxval=k_max))
-        k = k_max
-        actions = jax.random.randint(act_rng, shape=(k,), minval=0, maxval=num_actions)
+        rng, sub_rng = jax.random.split(rng)
+        actions = jax.random.randint(sub_rng, shape=(k,), minval=0, maxval=num_actions)
         states.extend((cube.step(act)[0] for act in actions))
         w.extend((1 / (d+1) for d in range(k)))
 
@@ -85,12 +84,12 @@ def make_targets(children, rewards, params):
     batch_size = 1000
     vals = []
     for i in range(ceil(children.shape[0] / batch_size)):
-        v, _ = apply_fun(params, children[i * batch_size : (i + 1) * batch_size])
+        v = apply_fun(params, children[i * batch_size : (i + 1) * batch_size])
         vals.append(v)
 
     # Add rewards to make target values.
     vals = jnp.vstack(vals).reshape(rewards.shape) + rewards
-    return jnp.max(vals, axis=1), jnp.argmax(vals, axis=1)
+    return jnp.max(vals, axis=1)
 
 
 def batch_generator(rng, data, batch_size):
@@ -105,8 +104,28 @@ def batch_generator(rng, data, batch_size):
         rng, sub_rng = jax.random.split(rng)
         idxs = jax.random.choice(sub_rng, jnp.arange(num_train), shape=(batch_size,), replace=False)
         yield (data["X"][idxs],
-               (data["y"][0][idxs], data["y"][1][idxs]),
+               data["y"][idxs],
                data["w"][idxs])
+
+
+
+#------------------------ utility functions ------------------------#
+def fib(n, memo={}):
+    """ Return the n-th Fibonacci number. """
+    if n == 0 or n == 1:
+        memo[n] = 1
+    elif n not in memo:
+        memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+
+def reverse_fib(n):
+    """ Return the index of the greatest number from the Fibonacci sequence,
+    that is smaller than or equal to n. """
+    i = 0
+    while fib(i+1) <= n:
+        i += 1
+    return i
 
 
 
@@ -125,8 +144,6 @@ opt_init, opt_update, get_params = optimizers.adam(step_size=step_fn)
 init_fun, apply_fun = model_fn()
 
 
-
-#-------------------- params training utilities --------------------#
 @jax.jit
 def l2_regularizer(params, reg=1e-4):
     """ Return the L2 regularization loss. """
@@ -137,35 +154,26 @@ def l2_regularizer(params, reg=1e-4):
 @jax.jit
 def loss_fn(params, batch):
     """ Return the total loss computed for a given batch. """
-    X, (y_vals, y_acts), w = batch
-    vhead_out, _ = apply_fun(params, X)
-    mse_loss = jnp.sum(((vhead_out - y_vals) ** 2).squeeze() * w)
+    X, y, w = batch
+    vals = apply_fun(params, X)
+    mse_loss = jnp.sum(((vals - y) ** 2).squeeze() * w)
     l2_loss = l2_regularizer(params)
     return mse_loss + l2_loss
 
 
-# @jax.jit
-# def update(i, opt_state, batch):
-#     """ Perform backpropagation and parameter update. """
-#     params = get_params(opt_state)
-#     loss, grads = jax.value_and_grad(loss_fn)(params, batch)
-#     # clip_grads produces different result when under jit !!!!!!
-#     # grads = optimizers.clip_grads(grads, max_norm=clip_norm)
-#     return loss, opt_update(i, grads, opt_state)
-
-
+@jax.jit
 def update(i, opt_state, batch):
     """ Perform backpropagation and parameter update. """
     params = get_params(opt_state)
     loss, grads = jax.value_and_grad(loss_fn)(params, batch)
-    grads = optimizers.clip_grads(grads, max_norm=5.0)
-    opt_state = jax.jit(opt_update)(i, grads, opt_state)
-    return loss, opt_state
+    # clipped_grads = optimizers.clip_grads(grads, clip_norm)   # norm clipping produces very big differences when jit.
+    clipped_grads = tree_map(lambda w: jnp.clip(w, -10.0, 10.0), grads)
+    return loss, opt_update(i, clipped_grads, opt_state)
 
 
-def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
-          num_iterations=21, num_samples=100, print_every=10, episodes=100,
-          k_max=10, clip_norm=5.0, verbose=False, params_save_path=None):
+def train(rng, env, batch_size=128, num_epochs=5, num_iterations=21,
+          num_samples=100, print_every=10, episodes=100, k_min=1, k_max=25,
+          verbose=False, params_save_path=None):
     """
     Train the model function by generating simulations of random-play.
     On every epoch generate a new simulation and run multiple iterations.
@@ -176,14 +184,14 @@ def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
 
     @param rng (PRNGKey): A pseudo-random number generator.
     @param env (Cube Object): A Cube object representing the environment.
-    @param reg (float): A scalar giving the L2-regularization strength.                                     [optional]
     @param batch_size (int): Size of minibatches used to compute loss and gradient during training.         [optional]
     @param num_epochs (int): The number of epochs to run for during training.                               [optional]
     @param num_iterations (int): The number of iterations through the generated episodes.                   [optional]
     @param num_samples (int): The number of times the dataset is reused.                                    [optional]
     @param print_every (int): An integer. Training progress will be printed every `print_every` iterations. [optional]
     @param episodes (int): Number of episodes to be created.                                                [optional]
-    @param k_max (int): Maximum length of backward moves.                                                   [optional]
+    @param k_min (int): Minimum length of sequence of backward moves.                                       [optional]
+    @param k_max (int): Maximum length of sequence of backward moves.                                       [optional]
     @param clip_norm (float): A scalar for gradient clipping.                                               [optional]
     @param verbose (bool): If set to false then no output will be printed during training.                  [optional]
     @param params_save_path (str): File path to save the model parameters.                                  [optional]
@@ -195,16 +203,22 @@ def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
     # Initialize model parameters and optimizer state.
     rng, init_rng = jax.random.split(rng)
     input_shape = env.terminal_state.shape
-    _, params = init_fun(init_rng, input_shape)
-    opt_state = opt_init(params)
+    params = None
+    if params_save_path is None:
+        _, params = init_fun(init_rng, input_shape)
+    else:
+        params = list(jnp.load(params_save_path, allow_pickle=True))
 
     # Begin training.
     for e in range(num_epochs):
         tic = time.time()
+        opt_state = opt_init(params)
 
         # Generate data from random-play using the environment.
         rng, sub_rng = jax.random.split(rng)
-        states, w, children, rewards = generate_episodes(sub_rng, env, episodes, k_max)
+        # k = max(k_min, min(k_max, reverse_fib(e + 1)))  # Slowly increase the length of each episode starting from k_min.
+        k = k_max
+        states, w, children, rewards = generate_episodes(sub_rng, env, episodes, k)
 
         # Train the model on the generated data. Periodically recompute the target values.
         epoch_mean_loss = 0.0
@@ -213,16 +227,16 @@ def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
 
             # Make targets for the generated episodes using the most recent params and build a batch generator.
             params = get_params(opt_state)
-            tgt_vals, tgt_acts = make_targets(children, rewards, params)
-            data = {"X" : states, "y" : (tgt_vals, tgt_acts), "w" : w}
+            tgt_vals = make_targets(children, rewards, params)
+            data = {"X" : states, "y" : tgt_vals, "w" : w}
             rng, sub_rng = jax.random.split(rng)
             train_batches = batch_generator(sub_rng, data, batch_size)
 
             # Run through the dataset and update model params.
             total_loss = 0.0
-            for _ in range(num_samples):
+            for i in range(num_samples):
                 batch = next(train_batches)
-                loss, opt_state = update(e, opt_state, batch)
+                loss, opt_state = update(it* num_samples + i, opt_state, batch)
                 total_loss += loss
 
             # Book-keeping.
@@ -230,11 +244,11 @@ def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
             epoch_mean_loss = (it * epoch_mean_loss + iter_mean_loss) / (it + 1)
             loss_history.append(iter_mean_loss)
 
+            # Printout results.
             toc_it = time.time()
             if it % print_every == 0 and verbose:
                 print("\t(Iteration({}/{}) took {:.3f} seconds) iter_mean_loss = {:.3f}".format(
-                                                                it + 1, num_iterations, (toc_it-tic_it), iter_mean_loss))
-
+                                                        it + 1, num_iterations, (toc_it-tic_it), iter_mean_loss))
         # Store the model parameters.
         if params_save_path is not None:
             jnp.save(params_save_path, params)
@@ -245,44 +259,27 @@ def train(rng, env, reg=0.0, batch_size=128, num_epochs=5,
         # Printout results.
         if verbose:
             print("(Epoch ({}/{}) took {:.3f} seconds), epoch_mean_loss = {:.3f}".format(
-                                                        e + 1, num_epochs, (toc - tic), epoch_mean_loss))
+                                                        e + 1, num_epochs, (toc-tic), epoch_mean_loss))
     return params, loss_history
 
 
 
 if __name__ == "__main__":
-
     from cube_model_naive import Cube as env
 
     seed = 0
     rng = jax.random.PRNGKey(seed=seed)
 
-    ### hyperparameters
-    reg = 0.0
-    batch_size = 16
-    num_epochs = 3
-    num_iterations = 5
-    num_samples = 20
-    print_every = 1
-    episodes = 100
-    k_max = 5
-    clip_norm = 0.01
-    verbose = True
-    params_save_path = None
-
-
     ### Run training.
     params, loss_history = train(rng, env,
-                                 reg=reg,
-                                 batch_size=batch_size,
-                                 num_epochs=num_epochs,
-                                 num_iterations=num_iterations,
-                                 num_samples=num_samples,
-                                 print_every=print_every,
-                                 episodes=episodes,
-                                 k_max=k_max,
-                                 clip_norm=clip_norm,
-                                 verbose=verbose,
-                                 params_save_path=params_save_path)
+                                 batch_size=128,
+                                 num_epochs=1,
+                                 num_iterations=1,
+                                 num_samples=50,
+                                 print_every=1,
+                                 episodes=1000,
+                                 k_max=15,
+                                 verbose=True,
+                                 params_save_path=None)
 
 #
